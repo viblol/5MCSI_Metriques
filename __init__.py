f51d3b508f0dfb79f0f912df9b63a8ee54f9870c
viblol <45266768+viblol@users.noreply.github.com>
2024-02-12 09:58:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 def hello_world():
     return render_template('hello.html')
 
-@app.route("/contact/")
+@app.route("/contact1/")
 def MaPremiereAPI():
     return "<h2>Ma page de contact</h2>"
 
@@ -34,6 +34,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
+
+@app.route("/contact/")
+def moncontact():
+    return render_template("contact.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
